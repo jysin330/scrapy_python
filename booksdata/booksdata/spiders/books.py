7e9ed389d5c022c5e1657813445f27e72aa2d507
Jyoti <jyotisingh82000@gmail.com>
2024-03-15 17:57:14 +0530

    Crawling the html content of any given url

--- booksdata/booksdata/spiders/books.py
+++ booksdata/booksdata/spiders/books.py
@@ -1,10 +1,21 @@
 import scrapy
-
+from pathlib import Path
 
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["toscrape.com"]
     start_urls = ["https://toscrape.com"]
 
+    def start_requests(self):
+        urls = [
+            "https://books.toscrape.com/catalogue/category/books/travel_2/index.html",
+            "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html",
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse) # automatically run the parse method
+
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = f"books-{page}.html"
+        Path(filename).write_bytes(response.body) # for writing the file and response.body gives the html content of  page
+        self.log(f"Saved file {filename}")
